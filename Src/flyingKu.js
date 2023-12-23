ee64bad3346f906718acdb8696397e6d217a3a38
clice lee <cliceleee@gmail.com>
2023-12-24 01:34:00 +0900

    add rocket, and display the rocket

--- Src/flyingKu.js
+++ Src/flyingKu.js
@@ -1,16 +1,45 @@
 /* ******* background ****** */
 let board;
-let boardwidth=360;
+let boardWidth=360;
 let boardHeight=640;
 let context;
 
+/* ******* rocket ****** */
+let rocketWidth=95;
+let rocketHeight=60;
+let rocket_X=boardWidth/8;
+let rocket_Y=boardHeight/2;
+let rocketImg;
 
+let rocket={
+    x: rocket_X,
+    y: rocket_Y,
+    width: rocketWidth,
+    height: rocketHeight
+}
+
+let pipeArray = [];
+let pipeWidth = 64; //width/height ratio = 384/3072 = 1/8
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
 
 window.onload=function(){
     board=document.getElementById("board");
     board.height=boardHeight;
-    board.width=boardwidth;
+    board.width=boardWidth;
     context=board.getContext("2d");
 
-
+    //rocket
+    rocketImg = new Image();
+    rocketImg.src = "../Assets/Ku-default.PNG";
+    rocketImg.onload = function() {
+        context.drawImage(rocketImg, rocket.x, rocket.y, rocket.width, rocket.height);
+    }
 }
+
+
+
